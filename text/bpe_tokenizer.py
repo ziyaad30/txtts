375772beefd952aaeabae383103479eada94a42a
Ziyaad <xavierbaatjes@yahoo.com>
2024-01-13 03:57:16 +0200

    Update bpe_tokenizer.py

--- text/bpe_tokenizer.py
+++ text/bpe_tokenizer.py
@@ -6,7 +6,7 @@ from tokenizers import Tokenizer
 
 from text.cleaners import english_cleaners
 
-DEFAULT_VOCAB_FILE = "../data/tokenizer.json"
+DEFAULT_VOCAB_FILE = None
 
 
 def remove_extraneous_punctuation(word):
