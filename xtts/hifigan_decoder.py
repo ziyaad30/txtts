4b43b0b1f9261d00b12a44cf7907112201787fa5
Ziyaad <xavierbaatjes@yahoo.com>
2024-01-13 03:42:14 +0200

    Update hifigan_decoder.py

--- xtts/hifigan_decoder.py
+++ xtts/hifigan_decoder.py
@@ -3,7 +3,7 @@ import torchaudio
 from torch import nn
 from torch.nn import Conv1d, ConvTranspose1d
 from torch.nn import functional as F
-from torch.nn.utils.parametrizations import weight_norm
+from torch.nn.utils import weight_norm
 from torch.nn.utils.parametrize import remove_parametrizations
 
 from xtts.utils.io import load_fsspec
